4a6b0dd4f532e9760709d2bfee762c01d202fd98
Thiago Petrone <thiago.petrone@gmail.com>
2016-06-06 01:38:49 -0300

    Add token encryption/decryption functionalities

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -1,17 +1,21 @@
 class ApiClient < ApplicationRecord
+  attr_accessor :unsecure_token, :decrypted_token
+
   validates :encrypted_token, uniqueness: true
   validates :name, presence: true
 
-  # The block below has academic purposes and could be replaced
-  # by a single line of code since Rails 5:
-  # has_secure_token :encrypted_token
-
   before_create :generate_encrypted_token
+  after_find :decrypt_token
 
   def generate_encrypted_token
     self.encrypted_token = loop do
-      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      self.unsecure_token = SecureRandom.base64.tr('+/=', 'Qrt')
+      token = BCrypt::Password.create(unsecure_token)
       break token unless ApiClient.where(encrypted_token: token).exists?
     end
+  end
+
+  def decrypt_token
+    self.decrypted_token = BCrypt::Password.new(encrypted_token)
   end
 end
