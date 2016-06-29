7ccc7373d13508d6c0a0893259087af48f69aee1
Thiago Petrone <thiago.petrone@gmail.com>
2016-06-29 18:53:51 -0300

    Remove devise_token_auth controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    confirmations:  'overrides/confirmations',
+    passwords: 'overrides/passwords'
+  }
   # For details on the DSL available within this file, see:
   # http://guides.rubyonrails.org/routing.html.
 
