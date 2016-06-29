725fda2a5e364a96c1e1adb37b1d6acc87db1993
Hans Ccacyahuillca <hansbecc@hotmail.com>
2016-06-29 22:54:34 +0000

    Update Spec Incident test issues/#i20

--- spec/requests/v1/incident_controller_spec.rb
+++ spec/requests/v1/incident_controller_spec.rb
@@ -3,48 +3,86 @@ RSpec.describe V1::IncidentsController, type: :request do
     { 'Accept' => 'application/vnd.smartcityplatform; version=1' }
   }
 
-  let(:api_client)            { APIClient.first }
-  let(:default_params_last)   { { spot: 1, token: api_client.token } }
-  let(:default_params_create) { { incident: { user: User.first.id,
-                                              spot: Spot.first.id,
-                                              category: 0,
-                                              comment: 'comment'
-                                            },
-                                  token: api_client.token
-                              } }
+  let(:default_params_create) {
+    {
+      incident: {
+        spot_id: Spot.first.id,
+        category: 1,
+        comment: 'comment'
+      }
+    }
+  }
 
-  describe 'incidents last' do
-    context 'with correct parameters' do
+  before(:each) do
+    @resource = build(:user)
+    @resource.skip_confirmation!
+    @resource.save!
+
+    @auth_headers = @resource.create_new_auth_token
+
+    @token     = @auth_headers['access-token']
+    @client_id = @auth_headers['client']
+    @expiry    = @auth_headers['expiry']
+
+    # Ensure that request is not treated as batch request
+    DeviseTokenAuthSpecHelper.age_token(@resource, @client_id)
+  end
+
+  describe 'GET index incident spot' do
+    context 'with correct parameter' do
       it 'succeeds' do
-        get '/incidents/last',
-            params: default_params_last,
-            headers: api_header
+        get '/incidents/' + Spot.first.id.to_s,
+          headers: api_header
 
         expect(response.content_type).to eq('application/json')
         expect(response).to have_http_status(:success)
-      end
-    end
-
-    context 'with invalid parameters' do
-      it 'returns an error' do
-        get '/incidents/last',
-            params: { token: api_client.token },
-            headers: api_header
-
-        expect(response).to have_http_status(:bad_request)
       end
     end
   end
 
-  describe 'incidents create' do
+  describe 'POST create incident spot' do
     context 'with correct parameters' do
-      it 'succeeds' do
+      it 'returns ok' do
         post '/incidents',
-             params: default_params_create,
-             headers: api_header
+          params: default_params_create,
+          headers: api_header.merge(@auth_headers)
 
         expect(response.content_type).to eq('application/json')
-        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with incorrect spot parameter' do
+      it 'returns error when a spot is incorrect' do
+        post '/incidents',
+          params: {
+            incident: {
+              spot_id: 's',
+              category: 0,
+              comment: 'comment'
+            }
+          },
+          headers: api_header.merge(@auth_headers)
+
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'with incorrect category parameter' do
+      it 'returns error' do
+        post '/incidents',
+          params: {
+            incident: {
+              spot_id: Spot.first.id,
+              category: -1,
+              comment: 'comment'
+            }
+          },
+          headers: api_header.merge(@auth_headers)
+
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
